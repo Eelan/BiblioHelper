eb4eff430338495afdc739ec53bb4952af9b395b
Eelan <jansens.pub@gmail.com>
2016-04-08 15:50:51 +0200

    domain_classes Update

--- app/src/main/java/com/github/eelan/bibliohelper/data/domain/domain_classes.kt
+++ app/src/main/java/com/github/eelan/bibliohelper/data/domain/domain_classes.kt
@@ -1,5 +1,8 @@
 package com.github.eelan.bibliohelper.data.domain
 
+import android.os.Parcel
+import android.os.Parcelable
+
 
 /**
  * Created by Jansens on 16/03/2016.
@@ -10,4 +13,16 @@ data class BookList(val booksList: List<Book>) {
     val size = booksList.size
 }
 
-data class Book(val idBook: String, val title: String, val author: String, val cover: String, val description: String)
+data class Book(val idBook: String, val title: String, val author: String, val cover: String, val description: String) : Parcelable {
+    constructor(source: Parcel) : this(source.readString(), source.readString(), source.readString(), source.readString(), source.readString())
+
+    override fun writeToParcel(dest: Parcel?, flags: Int) {
+        dest?.writeString(idBook);
+        dest?.writeString(title);
+        dest?.writeString(author);
+        dest?.writeString(cover);
+        dest?.writeString(description);
+    }
+
+    override fun describeContents(): Int = 0
+}
